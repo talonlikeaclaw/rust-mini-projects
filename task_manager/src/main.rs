2349d4821f41f4c911af84145b83545382883f3a
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-08-13 21:24:31 -0400

    import hashset and implement task struct

--- task_manager/src/main.rs
+++ task_manager/src/main.rs
@@ -1,3 +1,13 @@
+use std::collections::HashSet;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[derive(Debug)]
+struct Task {
+    id: u32,
+    name: String,
+    description: String,
+    tags: HashSet<String>,
 }
