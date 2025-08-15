c80f6849bd41f38c7e6683a305fabf8ee663ae7a
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-08-15 14:59:01 -0400

    add test code to test json file presistence (it works!)

--- task_manager/src/main.rs
+++ task_manager/src/main.rs
@@ -11,19 +11,36 @@ mod prelude {
 }
 
 fn main() {
+    let path = "tasks.json";
+    let mut repo: TaskRepo = match TaskRepo::load_from_path(path) {
+        Ok(res) => res,
+        Err(_) => TaskRepo::new(),
+    };
+
     // Testing
-    let mut repo: TaskRepo = TaskRepo::new();
-    repo.add_task(
-        "Hello World!".to_string(),
-        "Testing 123..".to_string(),
-        vec!["test".to_string(), "note".to_string()],
-    );
-    match repo.complete_task(1) {
-        Ok(_) => println!("Task marked as done!"),
-        Err(e) => println!("Error: {}", e),
-    }
-    let tasks: Vec<&Task> = repo.list_tasks();
-    if let Some(task) = tasks.first() {
+    // repo.add_task(
+    //     "Second!".to_string(),
+    //     "Am I able to save to JSON?".to_string(),
+    //     vec!["test".to_string(), "persists".to_string()],
+    // );
+
+    // match repo.complete_task(1) {
+    //     Ok(_) => println!("Task marked as done!"),
+    //     Err(e) => println!("Error: {}", e),
+    // }
+
+    if let Some(task) = repo.list_tasks().get(0) {
         println!("{:?}", task);
+    }
+
+    if let Some(task) = repo.list_tasks().get(1) {
+        println!("{:?}", task);
+    }
+
+    // Persist to file
+    if let Err(e) = repo.save_to_path(path) {
+        eprint!("Failed to save tasks: {}", e);
+    } else {
+        println!("Saved tasks to {}", path);
     }
 }
