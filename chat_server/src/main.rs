adf1ccda8538922ba04e4070febe714e034714ec
talonlikeaclaw <talonlikeaclaw@proton.me>
2025-08-18 12:43:25 -0400

    add cargo lock and implement example from tcplistener docs

--- chat_server/src/main.rs
+++ chat_server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    // ...
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:5095")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
 }
